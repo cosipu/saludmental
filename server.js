2bec32c70362eb00a091602b002eaccd63d77a4c
cosipu <cogotesinpluma@gmail.com>
2025-10-06 08:48:49 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -78,5 +78,9 @@ const db = new Low(adapter);
   app.get("/", (req, res) => res.sendFile(path.join(process.cwd(), "public/index.html")));
   app.get("/admin/bookings", (req, res) => res.sendFile(path.join(process.cwd(), "public/admin.html")));
 
-  app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
+ const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 })();
